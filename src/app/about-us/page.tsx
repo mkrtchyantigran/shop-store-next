cc03c5e812404c9e831784fc432cb34d061e5d82
mkrtchyantigran <tigran.mkrtchyan1@outlook.com>
2026-05-15 00:15:37 +0200

    add main banner section and About Us description section in page.tsx, and add for the styles in about-us.scss

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import Image from "next/image"
+import "./about-us.scss";
+import Link from "next/link";
 
 export default function AboutUs() {
   return (
-    <div>
-      AboutUs
-    </div>
+    <>
+      <section className='about-hero'>
+        <Image
+          src="/images/about-us/modern-room-decor.jpg"
+          alt=""
+          fill
+          priority
+          sizes="100vw"
+        />
+      </section>
+
+      <section className="about-description">
+        <div className="container">
+          <h1>Lectus morbi eget justo tellus facilisi orci venenatis aliquet. Egestas rutrum volutpat pretium curabitur scelerisque.</h1>
+          <div className="about-description-columns">
+            <p>Mi tristique est nunc sapien orci tortor ac. Suspendisse leo et cursus pharetra tellus tincidunt. Risus nulla penatibus donec elementum nulla. Pellentesque ipsum consequat velit blandit vel ornare augue magna Suspendisse leo et cursus pharetra tellus tincidunt. Risus nulla penatibus donec elementum nulla. Pellentesque ipsum consequat velit blandit vel ornare augue magna</p>
+            <p>Suspendisse leo et cursus pharetra tellus tincidunt. Risus nulla penatibus donec elementum nulla. Est nulla tincidunt commodo massa. Nunc interdum cras id augue. Tortor cras pharetra ac dignissim hendrerit fames fermentum. Mi tristique est nunc sapien orci tortor ac.</p>
+            <Link href="#" className="about-description-read-more">READ MORE</Link>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
